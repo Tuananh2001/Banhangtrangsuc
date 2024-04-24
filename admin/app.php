0d4107678ba28b5309da5b557cf1f8c32f54b691
Nguyen Tien Tuan Anh <nguyentientuananh2k1@gmail.com>
2024-04-24 17:10:37 +0700

    Add new project ecommerce

---
+++ admin/app.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../accset/css/app.css">
+    <title>Document</title>
+</head>
+<body>
+    <form style="margin-left: 10px;" method="POST" enctype="multipart/form-data">
+        <input class="top" type="file" name="file">
+        <button class="btn btn-primary" type="submit" name="app">App</button>
+    </form>
+</body>
+</html>
